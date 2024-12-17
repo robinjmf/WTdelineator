05c73f2e41d677940511b17d21f7fad8161b4f76
robinjmf <robn.jmf@gmail.com>
2024-12-17 18:04:37 +0100

    fix delineatesginal

--- delineateSignal.py
+++ delineateSignal.py
@@ -1,72 +1,93 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr 12 15:01:13 2018
-
-@author: carlos
-"""
-
 import matplotlib.pyplot as plt
 import WTdelineator as wav
 import wfdb
 import numpy as np
+import os
 
-
-dbase = 'staffiii/data'
+# Dataset Path and Record Name
+dbase = 'staff_data/data'
 rec = '052c'
 sNum = 1
 
-# # When in linux
-# s, att = wfdb.srdsamp(rec,pbdir=dbase)
-# annot = wfdb.rdann(rec, 'event', pbdir=dbase)
-# sName = att['signame']
+# Read Signal Data
+try:
+    s, att = wfdb.rdsamp(f"{dbase}/{rec}")
+    print("Signal loaded successfully.")
+except FileNotFoundError as e:
+    print(f"Signal loading error: {e}")
+    exit()
 
-# When in Windows
-s, att = wfdb.rdsamp(rec,pn_dir=dbase)
-annot = wfdb.rdann(rec, 'event', pn_dir=dbase)
-sName = att['sig_name']
+# Manually Handle `.event` File
+try:
+    # Assuming `.event` is a text file, read it as a fallback
+    event_file = os.path.join(dbase, f"{rec}.event")
+    with open(event_file, "r") as ef:
+        events = ef.readlines()
+    print("Event file loaded successfully.")
+    # Process `events` if necessary
+except FileNotFoundError as e:
+    print(f"Annotation loading error: {e}")
+    events = []
 
-# Ranges to analyse signal
-beg = int(np.floor(2**16))
-end = int(np.floor(2*2**16))
 
-#%%
+# Check available signals and select a valid signal index
+print(f"Available signals: {att['sig_name']}")
+print(f"Number of signals: {len(att['sig_name'])}")
+
+if sNum >= len(att['sig_name']):
+    print(f"Invalid signal number {sNum}, defaulting to the first signal.")
+    sNum = 0
+
+# Inspect attributes to identify signal name key
+print(f"Attributes in `att`: {att}")
+
+# Handle signal name dynamically
+if 'sig_name' in att:
+    sName = att['sig_name'][sNum]
+elif 'sig_name' in att:
+    sName = att['sig_name'][sNum]
+else:
+    print("Signal name not found in attributes. Using default name.")
+    sName = f"Signal {sNum}"
+
+print(f"Selected signal: {sName}")
+
+# Signal Metadata
 fs = att['fs']
-sig = s[beg:end,sNum]
+# sName = att.get('signame', ['ECG'])[sNum]
+
+# Range for Signal Analysis
+beg = int(np.floor(2 ** 16))
+end = int(np.floor(2 * 2 ** 16))
+sig = s[beg:end, sNum]
 N = sig.shape[0]
-t = np.arange(0,N/fs,1/fs)
+t = np.arange(0, N / fs, 1 / fs)
 
-# Wavelet Transform delineation
-Pwav, QRS, Twav = wav.signalDelineation(sig,fs)
+# Wavelet Transform Delineation
+Pwav, QRS, Twav = wav.signalDelineation(sig, fs)
 
-# Calculate biomarkers
-QRSd = QRS[:,-1] - QRS[:,0]
+# Calculate Biomarkers
+QRSd = QRS[:, -1] - QRS[:, 0]
+Tind = np.nonzero(Twav[:, 0])
+QT = Twav[Tind, -1] - QRS[Tind, 0]
+Td = Twav[Tind, -1] - Twav[Tind, 0]
+Pind = np.nonzero(Pwav[:, 0])
+Pd = Pwav[Pind, -1] - Pwav[Pind, 0]
 
-Tind = np.nonzero(Twav[:,0])
-QT = Twav[Tind,-1] - QRS[Tind,0]
-Td = Twav[Tind,-1] - Twav[Tind,0]
-
-Pind = np.nonzero(Pwav[:,0])
-Pd = Pwav[Pind,-1] - Pwav[Pind,0]
-
-
-
+# Plot Delineation Results
 plt.figure()
-plt.plot(t,sig,label=sName[sNum])
-plt.plot(t[QRS[:,0]],sig[QRS[:,0]],'*r',label='QRSon', markersize=15)
-plt.plot(t[QRS[:,1]],sig[QRS[:,1]],'*y',label='Q', markersize=15)
-plt.plot(t[QRS[:,2]],sig[QRS[:,2]],'*k',label='R', markersize=15)
-plt.plot(t[QRS[:,3]],sig[QRS[:,3]],'*m',label='S', markersize=15)
-plt.plot(t[QRS[:,4]],sig[QRS[:,4]],'*g',label='QRSend', markersize=15)
-plt.plot(t[Twav[:,0]],sig[Twav[:,0]],'^r',label='Ton', markersize=10)
-plt.plot(t[Twav[:,1]],sig[Twav[:,1]],'^k',label='T1', markersize=10)
-plt.plot(t[Twav[:,2]],sig[Twav[:,2]],'^m',label='T2', markersize=10)
-plt.plot(t[Twav[:,3]],sig[Twav[:,3]],'^g',label='Tend', markersize=10)
-#plt.plot(t[Pwav[:,0]],sig[Pwav[:,0]],'or',label='Pon', markersize=10)
-#plt.plot(t[Pwav[:,1]],sig[Pwav[:,1]],'ok',label='P1', markersize=10)
-#plt.plot(t[Pwav[:,2]],sig[Pwav[:,2]],'om',label='P2', markersize=10)
-#plt.plot(t[Pwav[:,3]],sig[Pwav[:,3]],'og',label='Pend', markersize=10)
+plt.plot(t, sig, label=sName)
+plt.plot(t[QRS[:, 0]], sig[QRS[:, 0]], '*r', label='QRSon', markersize=15)
+plt.plot(t[QRS[:, 1]], sig[QRS[:, 1]], '*y', label='Q', markersize=15)
+plt.plot(t[QRS[:, 2]], sig[QRS[:, 2]], '*k', label='R', markersize=15)
+plt.plot(t[QRS[:, 3]], sig[QRS[:, 3]], '*m', label='S', markersize=15)
+plt.plot(t[QRS[:, 4]], sig[QRS[:, 4]], '*g', label='QRSend', markersize=15)
+plt.plot(t[Twav[:, 0]], sig[Twav[:, 0]], '^r', label='Ton', markersize=10)
+plt.plot(t[Twav[:, 1]], sig[Twav[:, 1]], '^k', label='T1', markersize=10)
+plt.plot(t[Twav[:, 2]], sig[Twav[:, 2]], '^m', label='T2', markersize=10)
+plt.plot(t[Twav[:, 3]], sig[Twav[:, 3]], '^g', label='Tend', markersize=10)
 plt.title('Delineator output')
 plt.xlabel('Time (s)')
 plt.ylabel('ECG (mV)')
 plt.legend()
+plt.show()
